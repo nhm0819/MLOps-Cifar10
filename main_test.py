9df3d1dc5cfeec636bf76bf7f063fda7e59ba005
Hongmin <nhm9001@gmail.com>
2022-04-26 14:45:02 +0900

    add lint

--- main_test.py
+++ main_test.py
@@ -6,6 +6,7 @@ class MainTest(unittest.TestCase):
     def test_helloworld(self):
         ret = main.helloworld("Hong")
         self.assertEqual(ret, "Hello World! Hong!")
+        assert ret == "Hello World! Hong!"
 
 
 if __name__ == "__main__":
